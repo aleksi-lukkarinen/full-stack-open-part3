59849c76c840f94dfa078664eed146044cdcd734
Aleksi Lukkarinen <aleksi.lukkarinen@gmail.com>
2021-04-17 16:20:00 +0300

    Creates a Hello World server

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+const http = require("http")
 
-console.log("Hoi maailma!")
+const app = http.createServer((request, response) => {
+  response.writeHead(200, {"Content-Type": "text/plain"})
+  response.end("Hoi maailma!")
+})
+
+const port = 3001
+app.listen(port)
+
+console.log(`Phonebook server running on port ${port}.`)
