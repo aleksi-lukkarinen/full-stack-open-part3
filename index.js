e64c47f71677cdffbf131c819798e3b3860aabdf
Aleksi Lukkarinen <aleksi.lukkarinen@gmail.com>
2021-04-18 03:37:32 +0300

    Installs Cors

--- index.js
+++ index.js
@@ -122,6 +122,9 @@ function findEntryByUsingIdFrom(request) {
 const express = require("express")
 const app = express()
 
+const cors = require("cors")
+app.use(cors())
+
 app.use(express.json())
 
 const morgan = require("morgan")
